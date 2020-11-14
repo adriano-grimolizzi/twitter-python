d662a1d250f9989e35a714a868cb99d27446daff
Adriano Grimolizzi <a.grimolizzi@gmail.com>
2020-11-14 19:37:07 +0100

    returns json with length

--- server.py
+++ server.py
@@ -2,18 +2,21 @@ from flask import Flask, request
 
 app = Flask(__name__)
 
+
 @app.route("/tweets")
 def get():
     tweets = request.get_json()['tweets']
 
     handleTweets(tweets)
 
-    return 'You sent me ' + str(len(tweets)) + ' tweets!'
+    return {'length': str(len(tweets))}
+
 
 def handleTweets(tweets):
     for tweet in tweets:
         print("ID: ", tweet['id'])
         print("Text: ", tweet['text'])
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000, debug=True)
