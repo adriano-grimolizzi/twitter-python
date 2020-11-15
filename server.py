c1c8baad403196ec762ba7deb1a3c0d8eb0f27ff
Adriano Grimolizzi <a.grimolizzi@gmail.com>
2020-11-15 18:10:11 +0100

    returns random sentiment

--- server.py
+++ server.py
@@ -1,18 +1,32 @@
 from flask import Flask, request
+import random
 
 app = Flask(__name__)
 
 
-@app.route("/tweets")
-def get():
+@app.route("/tweets", methods = ['POST'])
+def handlePost():
+    print('Handling Request...')
+
     tweets = request.get_json()['tweets']
 
-    handleTweets(tweets)
+    logTweets(tweets)
 
-    return {'length': str(len(tweets))}
+    return {
+        'length': str(len(tweets)),
+        'sentiment': getSentiment(random.randint(0, 2))
+    }
 
 
-def handleTweets(tweets):
+def getSentiment(index):
+    return {
+        0: 'Mostly Positive',
+        1: 'Neutral',
+        2: 'Mostly Negative'
+    }[index]
+
+
+def logTweets(tweets):
     for tweet in tweets:
         print("ID: ", tweet['id'])
         print("Text: ", tweet['text'])
