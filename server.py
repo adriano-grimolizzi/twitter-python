78ed48c6b0c3d50d59cd995e0c70e666793ede4e
Enimsey <yesmine.najar@gmail.com>
2020-11-23 15:47:25 +0100

    feat(sentiment-analysis: detailed sentiments

--- server.py
+++ server.py
@@ -1,23 +1,28 @@
 from flask import Flask, request
 import random
-from utils import load_model, get_sentiment
+from utils import load_model, get_sentiment, predict_tweet
 
 app = Flask(__name__)
 
 model = load_model('model.pkl')
 
 @app.route("/tweets", methods = ['POST'])
-def handlePost():
+def handlePost(mode = "general"):
     print('Handling Request...')
 
     tweets = request.get_json()['tweets']
 
     logTweets(tweets)
-
-    return {
-        'length': str(len(tweets)),
-        'sentiment': getSentiment(tweets)
-    }
+    if mode == "general": # general sentiment
+        return {
+            'length': str(len(tweets)),
+            'overallSentiment': getSentiment(tweets)
+        }
+    else: # sentiment for each tweet
+        return {'length': str(len(tweets)),
+                'overallSentiment': getSentiment(tweets),
+                'analysedTweets': [{str(i): get_single_sentiment(tweets[i])} for i in range(len(tweets))]}
+        
 
 
 def getSentiment(tweets):
@@ -28,6 +33,13 @@ def getSentiment(tweets):
     }[general_sentiment]
 
 
+def get_single_sentiment(tweet):
+    sentiment = predict_tweet(tweet, model[1], model[0]) > 0.5
+    return {
+        0: 'Negative',
+        1: 'Positive'
+    }[sentiment[0,0]]
+    
 def logTweets(tweets):
     for tweet in tweets:
         print("ID: ", tweet['id'])
